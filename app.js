37bc70d2e5b15a9a234e0c0650ad37b539fb6b78
Jair Pérez <jair.perez@outlook.com>
2016-03-23 15:01:09 -0500

    Router avanzado

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 /* jshint node: true */
 
 var express = require("express");
-var mongoose = require("mongoose");
 var BodyParser = require("body-parser");
 var multer = require("multer");
 var cloudinary = require("cloudinary");
-var index = require("./routes/index.js");
-var birds = require("./routes/birds.js");
-var app_password = ("D1e560*9c");
+var Userdata = require('./bin/userdb').Userdata;
+var index = require("./routes/index");
+var router_app = require('./routes/users');
 var method_override = require("method-override");
 
 cloudinary.config({
@@ -18,19 +17,6 @@ cloudinary.config({
 
 var app = express();
 
-mongoose.connect("mongodb://localhost/userdata");
-
-var usuarioSchema = {
-  name:String,
-  lastname:String,
-  user:String,
-  email:String,
-  pass:String,
-  imageUrl:String
-};
-
-var Userdata = mongoose.model("Userdata",usuarioSchema);
-
 app.use(BodyParser.json());
 app.use(BodyParser.urlencoded({extended:true}));
 app.use(multer({dest: "./uploads"}));
@@ -38,29 +24,6 @@ app.use(method_override("_method"));
 
 app.set("view engine" , "jade");
 app.use(express.static('public'));
-
-app.get('/home',function(req,res){
-  res.render('home');
-});
-
-app.get('/horario',function(req,res){
-  res.render('perfil/estudiante/horario');
-});
-
-app.post("/admin",function(req,res){
-  if(req.body.contra === app_password){
-    Userdata.find(function(error,documento){
-      if(error){console.log(error);}
-      res.render("admin/index",{datos:documento});
-    });
-  }else{
-    res.redirect("/");
-  }
-});
-
-app.get("/admin",function(req,res){
-  res.render("admin/form");
-});
 
 app.get("/registro/edit/:id",function(req,res){
   var id_user = req.params.id;
@@ -87,120 +50,13 @@ app.put("/registro/:id", function(req,res){
   });
 });
 
-app.post("/registro",function(req,res){
-
-  var data = {
-    name: req.body.nombre,
-    lastname: req.body.apellido,
-    user: req.body.usuario,
-    email: req.body.correo,
-    pass: req.body.contraseña
-  };
-
-  var userdata = new Userdata(data);
-
-  userdata.save(function(){
-    console.log(userdata);
-    console.log(req.body.contraseña);
-    res.redirect("/");
-  });
-
-  /*cloudinary.uploader.upload(req.files.image_avatar.path,
-    function(result) {
-      userdata.imageUrl = result.url;
-      userdata.save(function(err){
-        console.log("userdata");
-        res.redirect("/");
-      });
-  });*/
-});
-
-app.get("/registro",function(req,res){
-  Userdata.find(function(error,documento){
-    if(error){console.log(error);}
-    res.render("registro/index",{datos:documento});
-  });
-});
-
-
-app.post("/home",function(req,res){
-  var user = req.body.user;
-  var pass = req.body.pass;
-
-  Userdata.findOne({"user": user,"pass": pass},function(error,perfiluser){
-    if(pass == perfiluser.pass){
-      console.log(perfiluser.pass);
-      res.render("home",{dto:perfiluser});
-    }else{
-      res.redirect("/");
-    }
-  });
-});
-
-app.get("/perfil",function(req,res){
-  res.render("perfil/estudiante/");
-});
-
-app.get("/perfil/actividad",function(req,res){
-  res.render("perfil/estudiante/actividad");
-});
-
-app.get("/perfil/notas" , function(req,res){
-  res.render("perfil/estudiante/periodo");
-});
-
-app.get("/perfil/profesor",function(req,res){
-  res.render("perfil/profesor");
-});
-
-app.get("/perfil/actividad/horario",function(req,res){
-  res.render("perfil/horario");
-});
-
-app.get("/perfil/actividad/periodo",function(req,res){
-  res.render("perfil/estudiante/periodo");
-});
-
-var profesor = function(req,res,next){
-  res.render('registro/profesor');
-  next();
-};
-
-var padre = function(req,res,next){
-  res.render('registro/padre');
-  next();
-};
-
-var estudiante = function(req,res){
-  res.render('registro/estudiante');
-};
-var angular = function(req,res){
-  res.render('angular');
-};
-
-app.get('/angular',angular);
-app.get('/registro/profesor',profesor);
-app.get('/registro/padre',padre);
-app.get('/registro/estudiante',estudiante);
-app.use("/", index);
-app.use("/birds", birds);
-
-app.get('/registro/:id/delete',function(req,res){
-  var id = req.params.id;
-  Userdata.findOne({'_id': id},function(err,userd){
-    res.render('registro/delete',{usd:userd});
-  });
-});
-
-app.get('/agregar-actividad',function(req,res){
-  res.render('agregar');
-});
-
-app.get('/buscar',function(req,res){
-  res.render('buscar');
-});
-
 app.delete('/menu/:id',function(req,res){
   console.log(res);
 });
+
+//ROUTERS
+app.use('/', index);
+app.use('/',router_app);
+
+//LISTEN
 app.listen(5000);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,43 @@
+/* jshint node: true */
 var express = require('express');
+var Userdata = require('../bin/userdb').Userdata;
 var router = express.Router();
+var app_password = ("D1e560*9c");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+//INDEX POST LOGIN
+router.post("/",function(req,res){
+  res.render('home');
+});
+
+//REGISTRO
+//REGISTRO EDITAR
+router.get('/:id/delete',function(req,res){
+  var id = req.params.id;
+  Userdata.findOne({'_id': id},function(err,userd){
+    res.render('registro/delete',{usd:userd});
+  });
+});
+
+//PERFIL
+
+//ADMIN
+router.get('/horario',function(req,res){
+  res.render('perfil/estudiante/horario');
+});
+
+router.post("/admin",function(req,res){
+  if(req.body.contra === app_password){
+    Userdata.find(function(error,documento){
+      if(error){console.log(error);}
+      res.render("admin/index",{datos:documento});
+    });
+  }else{
+    res.redirect("/");
+  }
+});
+
+router.get("/admin",function(req,res){
+  res.render("admin/form");
 });
 
 module.exports = router;
