e321dbebc77944c84f8de5df63a7cd3419d0c23e
Pro Estudio <Pro Estudio>
2016-04-24 19:41:22 -0500

    Proschool v0.6

--- server.js
+++ server.js
@@ -74,7 +74,6 @@ app.use(function(err, req, res, next) {
   console.log(next);
 });
 
-
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 app.listen(port, ip);
